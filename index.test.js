31cfdf53832aee024f279220c61b991d2fa6aaef
Nadya Serbouti <serbouti.nadya@gmail.com>
2025-02-03 22:29:05 +0100

    Initial commit

---
+++ index.test.js
@@ -0,0 +1,38 @@
+import { mockRequest, mockResponse } from "jest-mock-req-res";
+import { getClassicZodiacSign, getChineseZodiacSign } from "./index.js";
+
+describe("Getting my horoscope", () => {
+  it("gets accurate classic zodiac sign when given accurate query params", () => {
+    const req = mockRequest({ query: { day: "11", month: "11" } });
+    const res = mockResponse();
+    getClassicZodiacSign(req, res);
+    expect(res.json).toBeCalledWith({
+      data: "Your classic zodiac sign is Scorpio",
+    });
+  });
+
+  it("fails on classic zodiac sign when given invalid query params", () => {
+    const req = mockRequest({ query: { year: "1993" } });
+    const res = mockResponse();
+    getClassicZodiacSign(req, res);
+    expect(res.status).toBeCalledWith(400);
+    expect(res.send).toBeCalledWith("Please enter a valid day and month.");
+  });
+
+  it("gets accurate chinese zodiac sign when given accurate query params", () => {
+    const req = mockRequest({ query: { year: "1993" } });
+    const res = mockResponse();
+    getChineseZodiacSign(req, res);
+    expect(res.json).toBeCalledWith({
+      data: "Your chinese zodiac sign is Rooster",
+    });
+  });
+
+  it("fails on chinese zodiac sign when given invalid query params", () => {
+    const req = mockRequest({ query: { day: "11", month: "11" } });
+    const res = mockResponse();
+    getChineseZodiacSign(req, res);
+    expect(res.status).toBeCalledWith(400);
+    expect(res.send).toBeCalledWith("Please enter a valid year.");
+  });
+});
